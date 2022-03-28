fb183a2fa37f57054b03336b15ae9271df4d659b
Darren Nickerson <darren.nickerson@milcroft.com>
2022-03-28 10:37:54 +0100

    added database files

--- database-example/app.py
+++ database-example/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, url_for, redirect, request, flash
+from flask import Flask, render_template, url_for, redirect, request, flash, abort
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret key"
@@ -9,6 +9,16 @@ def get_db_connection():
     conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute("SELECT * FROM posts WHERE id = ?",
+                        (post_id)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 
 @app.route("/")
@@ -21,7 +31,42 @@ def home():
 
 @app.route("/create/", methods=("GET", "POST"))
 def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+
+        if not title:
+            flash("Title required.")
+        elif not content:
+            flash("Content required.")
+        else:
+            conn = get_db_connection()
+            conn.execute(
+                "INSERT INTO posts (title, content) VALUES (?, ?)", (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for("home"))
+
     return render_template("create.html")
+
+
+@app.route("/<int:id>/edit/", method=("GET", "POST"))
+def edit(id):
+    post = get_post(id)
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        if not title:
+            flash("Title required.")
+        elif not content:
+            flash("Content required.")
+        else:
+            conn = get_db_connection()
+            conn.execute(
+                "UPDATE posts (title, content) VALUES (?, ?)", (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
